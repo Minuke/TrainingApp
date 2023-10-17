59f42c821d56bbdedbc167c2b6df8f0d5721ca3c
Minuke <nakamadeath@gmail.com>
2023-10-17 16:15:18 +0200

    style adjustment

--- src/app/movies/components/card-movie/card-movie.component.ts
+++ src/app/movies/components/card-movie/card-movie.component.ts
@@ -1,20 +1,19 @@
-import { Component, Input } from "@angular/core";
-import { Router } from "@angular/router";
+import { Component, Input, OnChanges, SimpleChanges } from "@angular/core";
 import { Movie } from "src/app/interfaces/movie.interface";
 import { MoviesService } from "src/app/services/movies.service";
 
 @Component({
   selector: 'app-card-movie',
   templateUrl: './card-movie.component.html',
-  styleUrls: ['./card-movie.component.scss']
+  styleUrls: ['./card-movie.component.scss'],
+
 })
 export class CardMovieComponent {
 
   @Input() public movie:Movie;
   public visible:boolean = false;
-  public movieToDelete!:Movie;
 
-  constructor(private moviesService:MoviesService, private router:Router) {
+  constructor(private moviesService:MoviesService) {
     this.movie = {
       id: 0,
       title: '',
@@ -29,7 +28,7 @@ export class CardMovieComponent {
 
   deleteMovie(movieId:number):void {
     this.moviesService.deleteMovie(movieId).subscribe();
-    this.router.navigate(['/actors']);
+    location.reload();
   }
 
   showDialog():void {
--- src/app/movies/components/list-movies/list-movies.component.ts
+++ src/app/movies/components/list-movies/list-movies.component.ts
@@ -5,13 +5,12 @@ import { MoviesService } from "src/app/services/movies.service";
 @Component({
   selector: 'app-list-movies',
   templateUrl: './list-movies.component.html',
-  styleUrls: ['./list-movies.component.scss']
+  styleUrls: ['./list-movies.component.scss'],
 })
 export class ListMoviesComponent implements OnInit{
 
   public movies:Movie[] = [];
   public isLoading:boolean = true;
-
 
   constructor(private moviesService:MoviesService) {}
 
@@ -21,5 +20,5 @@ export class ListMoviesComponent implements OnInit{
       this.isLoading = false;
     });
   }
-
 }
+
